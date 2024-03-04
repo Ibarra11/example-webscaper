c5397106956076d27331bfa0c8c59da53d66cf8b
Ibarra11 <alanjibarradev@gmail.com>
2024-03-04 11:18:48 -0800

    add blurring effect to images

--- client/src/MainArticle.jsx
+++ client/src/MainArticle.jsx
@@ -5,8 +5,15 @@ export default function MainArticle() {
   return (
     <div className="min-h-screen bg-gray-700 py-12 px-8">
       <article className="max-w-2xl w-full mx-auto">
-        <div className="mb-4">
-          <img className="rounded-md" src={article.img.src} />
+        <a href="/" className="block text-gray-300 mb-4 text-sm">
+          Go Back
+        </a>
+        <div className="relative mb-4 w-full min-h-44 rounded-sm  overflow-hidden">
+          <img
+            className="absolute blur-[20px] scale-[2.1] w-full"
+            src={article.img.src}
+          />
+          <img className="relative w-fit mx-auto" src={article.img.src} />
         </div>
         <div className="mb-2 spacd-y-1 text-sm text-gray-300">
           <p>{article.author}</p>
