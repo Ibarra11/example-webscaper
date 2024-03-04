7af0d973a8cb015de78bcabd63e05f1c58c7ef3b
Ibarra11 <alanjibarradev@gmail.com>
2024-03-04 12:36:22 -0800

    change port variable

--- webscaper/index.js
+++ webscaper/index.js
@@ -4,7 +4,7 @@ import { readFile } from "fs/promises";
 import path from "path";
 import cors from "cors";
 const app = express();
-const port = process.env.port ?? 3000;
+const port = process.env.PORT ?? 3000;
 
 app.use(cors());
 
